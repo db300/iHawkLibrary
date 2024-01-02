9b7b7e85068da5533427fc2d6ef33ef2bb94531d
DavidHawkes <db300@163.com>
2024-01-02 14:49:01 +0800

    Update StringManager.cs

--- iHawkCommonLibrary/StringManager.cs
+++ iHawkCommonLibrary/StringManager.cs
@@ -30,7 +30,7 @@ namespace iHawkCommonLibrary
         /// </summary>
         /// <param name="command">命令行</param>
         /// <returns>解析后的参数列表</returns>
-        public static List<string> ParseArgs(string command)
+        public static List<string> ParseArgs(string command, List<string> specialStartTags, List<string> specialEndTags)
         {
             var ss = command.Split(' ').ToList();
             var cmdArgs = new List<string>();
@@ -38,21 +38,30 @@ namespace iHawkCommonLibrary
             while (ss.Count > 0)
             {
                 var s = ss[0];
+                var startTag = s.Substring(0, 1);
+                var endTag = s.Substring(s.Length - 1);
                 if (b)
                 {
-                    if (s.EndsWith("\""))
+                    if (specialEndTags.Contains(endTag))
                     {
-                        s = s.Trim('"');
+                        s = s.Substring(0, s.Length - 1);
                         b = false;
                     }
                     cmdArgs[cmdArgs.Count - 1] += $" {s}";
                 }
                 else
                 {
-                    if (s.StartsWith("\""))
+                    if (specialStartTags.Contains(startTag))
                     {
-                        s = s.Trim('"');
-                        b = true;
+                        s = s.Substring(1);
+                        if (specialEndTags.Contains(endTag))
+                        {
+                            s = s.Substring(0, s.Length - 1);
+                        }
+                        else
+                        {
+                            b = true;
+                        }
                     }
                     cmdArgs.Add(s);
                 }
