44c6a902ab77473c7144d3281aeedf3ccfdf82aa
DavidHawkes <db300@163.com>
2024-01-02 09:46:59 +0800

    新增项目

---
+++ iHawkCommonLibrary/StringManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iHawkCommonLibrary
+{
+    /// <summary>
+    /// 字符串管理器
+    /// </summary>
+    public static class StringManager
+    {
+        /// <summary>
+        /// 按照固定长度分割字符串
+        /// </summary>
+        /// <param name="str">待分割字符串</param>
+        /// <param name="maxLength">分割长度</param>
+        /// <returns>分割后的字符串列表</returns>
+        public static IEnumerable<string> SplitByLength(string str, int maxLength)
+        {
+            for (var index = 0; index < str.Length; index += maxLength)
+            {
+                yield return str.Substring(index, Math.Min(maxLength, str.Length - index));
+            }
+        }
+
+        /// <summary>
+        /// 解析命令行参数
+        /// </summary>
+        /// <param name="command">命令行</param>
+        /// <returns>解析后的参数列表</returns>
+        public static List<string> ParseArgs(string command)
+        {
+            var ss = command.Split(' ').ToList();
+            var cmdArgs = new List<string>();
+            var b = false;
+            while (ss.Count > 0)
+            {
+                var s = ss[0];
+                if (b)
+                {
+                    if (s.EndsWith("\""))
+                    {
+                        s = s.Trim('"');
+                        b = false;
+                    }
+                    cmdArgs[cmdArgs.Count - 1] += $" {s}";
+                }
+                else
+                {
+                    if (s.StartsWith("\""))
+                    {
+                        s = s.Trim('"');
+                        b = true;
+                    }
+                    cmdArgs.Add(s);
+                }
+                ss.RemoveAt(0);
+            }
+            return cmdArgs;
+        }
+    }
+}
